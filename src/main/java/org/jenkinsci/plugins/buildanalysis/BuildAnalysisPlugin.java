2a9c753764b9d1b83513be66b0839b6329832246
Vojtech Juranek <vjuranek@redhat.com>
2012-10-16 17:27:35 +0200

    Index plugin page

---
+++ src/main/java/org/jenkinsci/plugins/buildanalysis/BuildAnalysisPlugin.java
@@ -0,0 +1,14 @@
+package org.jenkinsci.plugins.buildanalysis;
+
+import hudson.Plugin;
+
+/**
+ * So far dummy class which server static content
+ * 
+ * @author vjuranek
+ *
+ */
+public class BuildAnalysisPlugin extends Plugin {
+	
+
+}
