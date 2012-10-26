77066236af4f20896065f1f1420c09af88c7d8ec
Vojtech Juranek <vjuranek@redhat.com>
2012-10-26 13:55:46 +0200

    Allow more M/R function for one collection

--- src/main/java/org/jenkinsci/plugins/buildanalysis/utils/MapReduceUtils.java
+++ src/main/java/org/jenkinsci/plugins/buildanalysis/utils/MapReduceUtils.java
@@ -7,12 +7,16 @@ import java.io.IOException;
 public class MapReduceUtils {
 
 	public static MapReduceFunctions getMapReduce(String collection) {
+    	return getMapReduce(collection, "");
+    }
+	
+	public static MapReduceFunctions getMapReduce(String collection, String name) {
     	String map = "";
     	String reduce = "";
     	ClassLoader classLoader = MapReduceUtils.class.getClassLoader();
     	try {
-    		map = IOUtils.toString(classLoader.getResourceAsStream("js/MapReduce/" + collection + "/map.js"));
-    		reduce = IOUtils.toString(classLoader.getResourceAsStream("js/MapReduce/" + collection + "/reduce.js"));
+    		map = IOUtils.toString(classLoader.getResourceAsStream("js/MapReduce/" + collection + "/map" + name + ".js"));
+    		reduce = IOUtils.toString(classLoader.getResourceAsStream("js/MapReduce/" + collection + "/reduce" + name + ".js"));
     	} catch(IOException e) {
     		e.printStackTrace();
     	}
