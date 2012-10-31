653280eb1d04e1267e2b2d71b1421c9f96c5e131
Vojtech Juranek <vjuranek@redhat.com>
2012-10-31 16:06:33 +0100

    not sure, what happened, some Eclipse change?

--- src/main/java/org/jenkinsci/plugins/buildanalysis/mongo/MongoBuildDAO.java
+++ src/main/java/org/jenkinsci/plugins/buildanalysis/mongo/MongoBuildDAO.java
@@ -19,115 +19,120 @@ import com.mongodb.MapReduceOutput;
 
 public class MongoBuildDAO implements BuildDAO {
 
-    private final DBCollection coll; 
-    
-    private static final String KEY_NUMBER = "number";
-    private static final String KEY_NAME = "name";
-    private static final String KEY_JOB_CLASS_NAME = "job_class";
-    private static final String KEY_JDK_NAME = "jdk_name";
-    private static final String KEY_LABEL = "label";
-    private static final String KEY_SCHEDULED_TIME = "scheduled_time";
-    private static final String KEY_STARTED_TIME = "started_time";
-    private static final String KEY_FINISHED_TIME = "finished_time";
-    private static final String KEY_RESULT = "result";
-    private static final String KEY_BUILD_ON = "build_on";
-    private static final String KEY_TRIGGER_CAUSES = "trigger_causes";
-    private static final String KEY_PARAMETERS = "parameters";
-    
-    public MongoBuildDAO(DBCollection coll) {
-    	this.coll = coll;
-    }
+	private final DBCollection coll;
 
-    public void create(BuildInfo build) {
-        BasicDBObject doc = new BasicDBObject();
-        doc.put(KEY_NUMBER,build.getNumber());
-        doc.put(KEY_NAME, build.getName());
-        doc.put(KEY_SCHEDULED_TIME, build.getScheduledTime());
-        coll.insert(doc);
-    }
-    
-    public void updateOnStarted(BuildInfo build) {
-        BasicDBObject old = new BasicDBObject();
-        old.put(KEY_NUMBER,build.getNumber());
-        old.put(KEY_NAME, build.getName());
-        BasicDBObject doc = new BasicDBObject();
-        doc.put(KEY_JOB_CLASS_NAME, build.getClassName());
-        doc.put(KEY_JDK_NAME,build.getJdkName());
-        doc.put(KEY_LABEL, build.getLabel());
-        doc.put(KEY_STARTED_TIME, build.getStartedTime());
-        doc.put(KEY_BUILD_ON, build.getBuildOn());
-        doc.put(KEY_TRIGGER_CAUSES, BuildUtils.convertCauses(build.getTriggerCauses()));
-        doc.put(KEY_PARAMETERS, build.getParameters());
-        coll.update(old, new BasicDBObject().append("$set", doc), true, false);
-    }
-    
-    public void updateOnFinalized(BuildInfo build) {
-        BasicDBObject old = new BasicDBObject();
-        old.put(KEY_NUMBER,build.getNumber());
-        old.put(KEY_NAME, build.getName());
-        BasicDBObject doc = new BasicDBObject();
-        doc.put(KEY_FINISHED_TIME, build.getFinishedTime());
-        doc.put(KEY_RESULT, BuildUtils.convertResult(build.getResult()));
-        coll.update(old, new BasicDBObject().append("$set", doc), true, false);
-    }
-    
-    public void getBuilds(String jobName) {
-    	BasicDBObject query = new BasicDBObject();
-    	query.put(KEY_NAME, jobName);
-    	List<DBObject> results = doQuery(query);
-    	
-    	for(DBObject o : results) {
-    		System.out.println(o);
-    	}
-    	
-    }
-    
-    public void getBuild(String jobName, int number) {
-    	BasicDBObject query = new BasicDBObject();
-    	query.put(KEY_NAME, jobName);
-    	query.put(KEY_NUMBER, number);
-    	List<DBObject> results = doQuery(query);
-    	
-    	for(DBObject o : results) {
-    		System.out.println(o);
-    	}
-    }
-    
-    
-    /**
-     * 
-     * @param query to be executed
-     * @return {@link List} of search results, can be empty but never null
-     */
-    private List<DBObject> doQuery(BasicDBObject query) {
-    	List<DBObject> results = new ArrayList<DBObject>();
-    	DBCursor cursor = coll.find(query);
-        try {
-            while(cursor.hasNext()) {
-                results.add(cursor.next());
-            }
-        } finally {
-            cursor.close();
-        }
-        return results;
-    }
-    
-    private MapReduceOutput mapReduce() {
-    	String map = "";
-    	String reduce = "";
-    	ClassLoader classLoader = getClass().getClassLoader();
-    	try {
-    		map = IOUtils.toString(classLoader.getResourceAsStream("js/MapReduce/mapTest.js"));
-    		reduce = IOUtils.toString(classLoader.getResourceAsStream("js/MapReduce/reduceTest.js"));
-    	} catch(IOException e) {
-    		e.printStackTrace();
-    	}
-    	
-    	MapReduceOutput out = coll.mapReduce(map, reduce, null, MapReduceCommand.OutputType.INLINE, null);
-    	for(DBObject o : out.results()) {
-    		System.out.println("Values: " + o.get("value"));
-    	}
-    	return out;
-    }
-    
+	private static final String KEY_NUMBER = "number";
+	private static final String KEY_NAME = "name";
+	private static final String KEY_JOB_CLASS_NAME = "job_class";
+	private static final String KEY_JDK_NAME = "jdk_name";
+	private static final String KEY_LABEL = "label";
+	private static final String KEY_SCHEDULED_TIME = "scheduled_time";
+	private static final String KEY_STARTED_TIME = "started_time";
+	private static final String KEY_FINISHED_TIME = "finished_time";
+	private static final String KEY_RESULT = "result";
+	private static final String KEY_BUILD_ON = "build_on";
+	private static final String KEY_TRIGGER_CAUSES = "trigger_causes";
+	private static final String KEY_PARAMETERS = "parameters";
+
+	public MongoBuildDAO(DBCollection coll) {
+		this.coll = coll;
+	}
+
+	public void create(BuildInfo build) {
+		BasicDBObject doc = new BasicDBObject();
+		doc.put(KEY_NUMBER, build.getNumber());
+		doc.put(KEY_NAME, build.getName());
+		doc.put(KEY_SCHEDULED_TIME, build.getScheduledTime());
+		coll.insert(doc);
+	}
+
+	public void updateOnStarted(BuildInfo build) {
+		BasicDBObject old = new BasicDBObject();
+		old.put(KEY_NUMBER, build.getNumber());
+		old.put(KEY_NAME, build.getName());
+		
+		BasicDBObject doc = new BasicDBObject();
+		doc.put(KEY_JOB_CLASS_NAME, build.getClassName());
+		doc.put(KEY_JDK_NAME, build.getJdkName());
+		doc.put(KEY_LABEL, build.getLabel());
+		doc.put(KEY_STARTED_TIME, build.getStartedTime());
+		doc.put(KEY_BUILD_ON, build.getBuildOn());
+		doc.put(KEY_TRIGGER_CAUSES,
+				BuildUtils.convertCauses(build.getTriggerCauses()));
+		doc.put(KEY_PARAMETERS, build.getParameters());
+		coll.update(old, new BasicDBObject().append("$set", doc), true, false);
+	}
+
+	public void updateOnFinalized(BuildInfo build) {
+		BasicDBObject old = new BasicDBObject();
+		old.put(KEY_NUMBER, build.getNumber());
+		old.put(KEY_NAME, build.getName());
+		BasicDBObject doc = new BasicDBObject();
+		doc.put(KEY_FINISHED_TIME, build.getFinishedTime());
+		doc.put(KEY_RESULT, BuildUtils.convertResult(build.getResult()));
+		coll.update(old, new BasicDBObject().append("$set", doc), true, false);
+	}
+
+	public void getBuilds(String jobName) {
+		BasicDBObject query = new BasicDBObject();
+		query.put(KEY_NAME, jobName);
+		List<DBObject> results = doQuery(query);
+
+		for (DBObject o : results) {
+			System.out.println(o);
+		}
+
+	}
+
+	public void getBuild(String jobName, int number) {
+		BasicDBObject query = new BasicDBObject();
+		query.put(KEY_NAME, jobName);
+		query.put(KEY_NUMBER, number);
+		List<DBObject> results = doQuery(query);
+
+		for (DBObject o : results) {
+			System.out.println(o);
+		}
+	}
+
+	/**
+	 * 
+	 * @param query
+	 *            to be executed
+	 * @return {@link List} of search results, can be empty but never null
+	 */
+	private List<DBObject> doQuery(BasicDBObject query) {
+		List<DBObject> results = new ArrayList<DBObject>();
+		DBCursor cursor = coll.find(query);
+		try {
+			while (cursor.hasNext()) {
+				results.add(cursor.next());
+			}
+		} finally {
+			cursor.close();
+		}
+		return results;
+	}
+
+	private MapReduceOutput mapReduce() {
+		String map = "";
+		String reduce = "";
+		ClassLoader classLoader = getClass().getClassLoader();
+		try {
+			map = IOUtils.toString(classLoader
+					.getResourceAsStream("js/MapReduce/mapTest.js"));
+			reduce = IOUtils.toString(classLoader
+					.getResourceAsStream("js/MapReduce/reduceTest.js"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		MapReduceOutput out = coll.mapReduce(map, reduce, null,
+				MapReduceCommand.OutputType.INLINE, null);
+		for (DBObject o : out.results()) {
+			System.out.println("Values: " + o.get("value"));
+		}
+		return out;
+	}
+
 }
