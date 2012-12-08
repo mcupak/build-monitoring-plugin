d6a1a625d5ac8e0ebe95dd8fc365bf6f99ffdcfa
Vojtech Juranek <vjuranek@redhat.com>
2012-12-09 00:14:16 +0100

    Build types plot

---
+++ src/main/resources/js/MapReduce/builds/mapBuildType.js
@@ -0,0 +1,3 @@
+function() {
+	emit(this.job_class, {count: 1});
+}
---
+++ src/main/resources/js/MapReduce/builds/reduceBuildType.js
@@ -0,0 +1,9 @@
+function(keys,values) { 
+	
+	var sum = 0 ;
+	values.forEach(function(value) {
+	      sum += value.count;
+	    });
+	
+	return sum;
+}
